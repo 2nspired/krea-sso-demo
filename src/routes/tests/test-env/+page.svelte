c319eda2b561b517e69dd3d6e9829e95b07a509d
Thomas Trudzinski <thomastrudzinski@gmail.com>
2025-05-17 18:34:59 -1000

    supabase added

---
+++ src/routes/tests/test-env/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+</script>
+
+<h1>Env Test</h1>
+
+<ul>
+	<li>Supabase URL: {PUBLIC_SUPABASE_URL === PUBLIC_SUPABASE_URL ? '✅' : '❌'}</li>
+	<li>Supabase Anon Key: {PUBLIC_SUPABASE_ANON_KEY === PUBLIC_SUPABASE_ANON_KEY ? '✅' : '❌'}</li>
+</ul>
