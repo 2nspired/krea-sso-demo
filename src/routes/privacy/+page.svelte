c92997e1526908d910f15a3198f4dae3359ecf76
Thomas Trudzinski <thomastrudzinski@gmail.com>
2025-05-17 20:28:49 -1000

    routes added for privacy and terms information required for google

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,7 @@
+<main class="min-h-screen bg-zinc-900 text-zinc-50">
+	<div class="container mx-auto">
+		<section class="flex flex-row items-center justify-center">
+			<p class="font-semi-bold mt-10">Privacy Policy</p>
+		</section>
+	</div>
+</main>
