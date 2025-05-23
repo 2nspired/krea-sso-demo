8fad6978667c29d67951c12f5b234611f4bfebe5
Thomas Trudzinski <thomastrudzinski@gmail.com>
2025-05-22 18:21:08 -1000

    debug tool

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -13,6 +13,18 @@
 </script>
 
 <main class="min-h-screen bg-gradient-to-b from-white to-zinc-100 text-zinc-900">
+	<pre class="text-xs">
+		{JSON.stringify(
+			{
+				isLoggedIn,
+				isSSO,
+				provider,
+				user: data.user
+			},
+			null,
+			2
+		)}
+		</pre>
 	{#if isLoggedIn}
 		<div class={`${isSSO ? 'bg-blue-500' : 'bg-orange-500'} px-4 py-2 text-white`}>
 			<div class="mx-auto flex w-full max-w-6xl items-center justify-between px-6 py-1">
